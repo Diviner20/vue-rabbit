6cb7d4b1310c718861c78d23323a11366046bdaa
Diviner20 <3237949626@qq.com>
2023-06-01 16:38:24 +0800

    home-图片懒加载优化

--- src/main.js
+++ src/main.js
@@ -8,37 +8,13 @@ import router from './router'
 
 //引入初始化样式文件
 import '@/styles/common.scss'
-import { useIntersectionObserver } from '@vueuse/core'
 
-//测试接口函数
-// import { getCategory } from './apis/testAPI'
-// getCategory().then(res => {
-//     console.log(res);
-// })
+//引入懒加载指令插件并且注册
+import { lazyPlugin } from '@/directives/index'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
-
-
-//定义全局指令
-app.directive('img-lazy', {
-    mounted(el, binding) {
-        //el: 指令绑定的元素 img
-        //binding: binging.value  指令等于号后面绑定的表达式的值 图片url
-        console.log(el, binding.value);
-        useIntersectionObserver(
-            el,
-            ([{ isIntersecting }], ) => {
-                if (isIntersecting) {
-                    // 进入视口区域
-                    el.src = binding.value
-                }
-            },
-        )
-
-    }
-})
